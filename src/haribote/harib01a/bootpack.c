4404ce8f5c85fa9c4215112a7107db73f308895b
Ubugeeei <ubuge1122@gamil.com>
2022-04-24 01:45:19 +0900

    feat: draw white screen

--- src/haribote/harib01a/bootpack.c
+++ src/haribote/harib01a/bootpack.c
@@ -1,7 +1,14 @@
 void io_hlt(void);
+void write_mem8(int addr, int data);
 
 void HariMain(void)
 {
+	int i;
+
+	for (i = 0xa0000; i <= 0xaffff; i++) { // メモリアドレス 0xa0000から0xaffffまで15で埋める
+		write_mem8(i, 15);
+	}
+
 	fin:
 		io_hlt();
 		goto fin;
--- src/haribote/harib01a/naskfunc.nas
+++ src/haribote/harib01a/naskfunc.nas
@@ -1,9 +1,15 @@
 [BITS 32]
 
-GLOBAL	io_hlt
+GLOBAL	io_hlt, write_mem8
 
 [SECTION .text]
 
 io_hlt:
     HLT
     RET
+
+write_mem8:
+	MOV     ECX,[ESP+4]
+	MOV     AL,[ESP+8]
+	MOV     [ECX],AL
+	RET
