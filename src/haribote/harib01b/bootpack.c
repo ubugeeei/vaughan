bb0e5a56dbde60b5c16620861cd6eb1193f871a3
Ubugeeei <ubuge1122@gamil.com>
2022-04-24 14:52:05 +0900

    feat: impl stripe screen without assemble function

---
+++ src/haribote/harib01b/bootpack.c
@@ -0,0 +1,22 @@
+void io_hlt(void);
+void write_mem8(int addr, int data)
+{
+	char *p;
+	p = (char *)addr;
+	*p = data;
+}
+
+void HariMain(void)
+{
+	int i;
+
+	for (i = 0xa0000; i <= 0xaffff; i++)
+	{
+		write_mem8(i, i & 0x0f);
+	}
+
+	for (;;)
+	{
+		io_hlt();
+	}
+}
---
+++ src/haribote/harib01b/naskfunc.nas
@@ -0,0 +1,9 @@
+[BITS 32]
+
+GLOBAL	io_hlt
+
+[SECTION .text]
+
+io_hlt:
+    HLT
+    RET
