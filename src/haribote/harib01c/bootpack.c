e12f1cf8e55d14b91a6b36d431d6e41c6eb962fd
Ubugeeei <ubuge1122@gamil.com>
2022-04-24 16:08:40 +0900

    feat: impl color palette

---
+++ src/haribote/harib01c/bootpack.c
@@ -0,0 +1,75 @@
+// assembly functions
+void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+// prototypes
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+
+void HariMain(void)
+{
+	int i;
+	char *p;
+
+	init_palette();
+
+	p = (char *)0xa0000;
+	for (i = 0; i <= 0xffff; i++)
+	{
+		p[i] = i / 4096;
+	}
+
+	for (;;)
+	{
+		io_hlt();
+	}
+}
+
+void init_palette(void)
+{
+	/* colors */
+	static unsigned char table_rgb[16 * 3] = {
+			// light
+			0x00, 0x00, 0x00, // black
+			0xff, 0x00, 0x00, // red
+			0x00, 0xff, 0x00, // green
+			0xff, 0xff, 0x00, // yellow
+			0x00, 0x00, 0xff, // blue
+			0xff, 0x00, 0xff, // purple
+			0x00, 0xff, 0xff, // light blue
+			0xff, 0xff, 0xff, // white
+			0xc6, 0xc6, 0xc6, // grey
+
+			// dark
+			0x84, 0x00, 0x00, // red
+			0x00, 0x84, 0x00, // green
+			0x84, 0x84, 0x00, // yellow
+			0x00, 0x00, 0x84, // blue
+			0x84, 0x00, 0x84, // purple
+			0x00, 0x84, 0x84, // light blue
+			0x84, 0x84, 0x84	// grey
+	};
+
+	set_palette(0, 15, table_rgb);
+	return;
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+	int i, eflags;
+	eflags = io_load_eflags();
+	io_cli();
+	io_out8(0x03c8, start);
+	for (i = start; i <= end; i++)
+	{
+		io_out8(0x03c9, rgb[0] / 4);
+		io_out8(0x03c9, rgb[1] / 4);
+		io_out8(0x03c9, rgb[2] / 4);
+		rgb += 3;
+	}
+	io_store_eflags(eflags);
+	return;
+}
