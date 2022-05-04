4845f2b27cc0c0e95a36856f00c5a6fc35054068
Ubugeeei <ubuge1122@gamil.com>
2022-05-04 17:31:32 +0900

    tweak: doc

--- src/haribote/harib03c/int.c
+++ src/haribote/harib03c/int.c
@@ -3,7 +3,7 @@
 void init_pic(void)
 {
 	io_out8(PIC0_IMR, 0xff); // no recieve interrupt
-	io_out8(PIC1_IMR, 0xff); //no recieve interruptい
+	io_out8(PIC1_IMR, 0xff); // no recieve interrupt
 
 	io_out8(PIC0_ICW1, 0x11);
 	io_out8(PIC0_ICW2, 0x20);
@@ -21,4 +21,5 @@ void init_pic(void)
 	io_out8(PIC1_IMR, 0xff);
 
 	return;
+
 }
