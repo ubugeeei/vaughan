ef36d54785d480a171f230c6fc841bb2a91eccff
Ubugeeei <ubuge1122@gamil.com>
2022-05-05 20:01:33 +0900

    feat: impl key buff queue

---
+++ src/haribote/harib04c/int.c
@@ -0,0 +1,53 @@
+#include "bootpack.h"
+
+void init_pic(void) {
+	io_out8(PIC0_IMR, 0xff);  // no recieve interrupt
+	io_out8(PIC1_IMR, 0xff);  // no recieve interrupt
+
+	io_out8(PIC0_ICW1, 0x11);
+	io_out8(PIC0_ICW2, 0x20);
+
+	io_out8(PIC0_ICW3, 0x01 << 0x02);
+	io_out8(PIC0_ICW4, 0x01);
+
+	io_out8(PIC1_ICW1, 0x11);
+	io_out8(PIC1_ICW2, 0x28);
+
+	io_out8(PIC1_ICW3, 0x02);
+	io_out8(PIC1_ICW4, 0x01);
+
+	io_out8(PIC0_IMR, 0xfb);
+	io_out8(PIC1_IMR, 0xff);
+
+	return;
+}
+
+#define PORT_KEYDAT 0x0060
+
+struct KEYBUF keybuf;
+
+void inthandler21(int *esp) {
+	unsigned char data;
+	io_out8(PIC0_OCW2, 0x61); /* IRQ-01��t������PIC�ɒʒm */
+	data = io_in8(PORT_KEYDAT);
+	if (keybuf.next < 32) {
+		keybuf.data[keybuf.next] = data;
+		keybuf.next++;
+	}
+	return;
+}
+
+void inthandler2c(int *esp) {
+	struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
+	boxfill8(binfo->vram, binfo->scrnx, COL8_000000, 0, 0, 32 * 8 - 1, 15);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF,
+		      "INT 2C (IRQ-12) : PS/2 mouse");
+	for (;;) {
+		io_hlt();
+	}
+}
+
+void inthandler27(int *esp) {
+	io_out8(PIC0_ICW2, 0x67);
+	return;
+}
