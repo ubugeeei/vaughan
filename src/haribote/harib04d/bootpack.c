c71cbb4aa850ce8d13ab6acadac7cafc2f183b23
Ubugeeei <ubuge1122@gamil.com>
2022-05-05 22:53:15 +0900

    feat: improve key buff queue

---
+++ src/haribote/harib04d/bootpack.c
@@ -0,0 +1,46 @@
+#include "bootpack.h"
+
+extern struct KEYBUF keybuf;
+
+void HariMain(void) {
+	struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
+	char s[40], mcursor[256];
+	int mx, my, i;
+
+	init_gdtidt();
+	init_pic();
+	io_sti();
+
+	io_out8(PIC0_IMR, 0xf9);
+	io_out8(PIC1_IMR, 0xef);
+
+	init_palette();
+	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
+	mx = (binfo->scrnx - 16) / 2;
+	my = (binfo->scrny - 28 - 16) / 2;
+	init_mouse_cursor8(mcursor, COL8_008484);
+	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+	sprintf(s, "(%d, %d)", mx, my);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+
+	for (;;) {
+		io_cli();
+		if (keybuf.len == 0) {
+			io_stihlt();
+		} else {
+			i = keybuf.data[keybuf.next_r];
+			keybuf.len--;
+			keybuf.next_r++;
+			if (keybuf.next_r == 32) {
+				keybuf.next_r = 0;
+			}
+			io_sti();
+			sprintf(s, "%x", i);
+			boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16,
+				 15, 31);
+			putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16,
+				      COL8_FFFFFF, s);
+		}
+	}
+}
+
