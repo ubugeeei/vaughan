1a4725de78f621afb2a1fc3a2c131ee085b08572
Ubugeeei <ubuge1122@gamil.com>
2022-05-29 23:28:01 +0900

    feat: impl memory test function

--- src/haribote/harib06b/bootpack.c
+++ src/haribote/harib06b/bootpack.c
@@ -1,7 +1,6 @@
 #include "bootpack.h"
 
-void HariMain(void)
-{
+void HariMain(void) {
 	struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
 	char s[40], mcursor[256], keybuf[32], mousebuf[128];
 	int mx, my, i;
@@ -31,83 +30,123 @@ void HariMain(void)
 
 	enable_mouse(&mdec);
 
-	for (;;)
-	{
+	for (;;) {
 		io_cli();
-		if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) == 0)
-		{
+		if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) == 0) {
 			io_stihlt();
-		}
-		else
-		{
-			if (fifo8_status(&keyfifo) != 0)
-			{
+		} else {
+			if (fifo8_status(&keyfifo) != 0) {
 				i = fifo8_get(&keyfifo);
 				io_sti();
 				sprintf(s, "%x", i);
 				boxfill8(binfo->vram, binfo->scrnx, COL8_008484,
-								 0, 16, 15, 31);
+					 0, 16, 15, 31);
 				putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16,
-											COL8_FFFFFF, s);
-			}
-			else if (fifo8_status(&mousefifo) != 0)
-			{
+					      COL8_FFFFFF, s);
+			} else if (fifo8_status(&mousefifo) != 0) {
 				i = fifo8_get(&mousefifo);
 				io_sti();
-				if (mouse_decode(&mdec, i) != 0)
-				{
+				if (mouse_decode(&mdec, i) != 0) {
 					sprintf(s, "[lcr %d %d]", mdec.x,
-									mdec.y);
-					if ((mdec.btn & 0x01) != 0)
-					{
+						mdec.y);
+					if ((mdec.btn & 0x01) != 0) {
 						s[1] = 'L';
 					}
-					if ((mdec.btn & 0x02) != 0)
-					{
+					if ((mdec.btn & 0x02) != 0) {
 						s[3] = 'R';
 					}
-					if ((mdec.btn & 0x04) != 0)
-					{
+					if ((mdec.btn & 0x04) != 0) {
 						s[2] = 'C';
 					}
 					boxfill8(binfo->vram, binfo->scrnx,
-									 COL8_008484, 32, 16,
-									 32 + 15 * 8 - 1, 31);
+						 COL8_008484, 32, 16,
+						 32 + 15 * 8 - 1, 31);
 					putfonts8_asc(binfo->vram, binfo->scrnx,
-												32, 16, COL8_FFFFFF, s);
+						      32, 16, COL8_FFFFFF, s);
 					boxfill8(binfo->vram, binfo->scrnx,
-									 COL8_008484, mx, my, mx + 15,
-									 my + 15);
+						 COL8_008484, mx, my, mx + 15,
+						 my + 15);
 					mx += mdec.x;
 					my += mdec.y;
 
-					if (mx < 0)
-					{
+					if (mx < 0) {
 						mx = 0;
 					}
-					if (my < 0)
-					{
+					if (my < 0) {
 						my = 0;
 					}
-					if (mx > binfo->scrnx - 16)
-					{
+					if (mx > binfo->scrnx - 16) {
 						mx = binfo->scrnx - 16;
 					}
-					if (my > binfo->scrny - 16)
-					{
+					if (my > binfo->scrny - 16) {
 						my = binfo->scrny - 16;
 					}
 
 					sprintf(s, "(%d, %d)", mx, my);
 					boxfill8(binfo->vram, binfo->scrnx,
-									 COL8_008484, 0, 0, 79, 15);
+						 COL8_008484, 0, 0, 79, 15);
 					putfonts8_asc(binfo->vram, binfo->scrnx,
-												0, 0, COL8_FFFFFF, s);
+						      0, 0, COL8_FFFFFF, s);
 					putblock8_8(binfo->vram, binfo->scrnx,
-											16, 16, mx, my, mcursor,
-											16);
+						    16, 16, mx, my, mcursor,
+						    16);
 				}
 			}
 		}
 	}
+}
+
+#define EFLAGS_AC_BIT 0x00040000
+#define CR0_CACHE_DISABLE 0x60000000
+
+unsigned int memtest(unsigned int start, unsigned int end) {
+	char flg486 = 0;
+	unsigned int eflg, cr0, i;
+
+	eflg = io_load_eflags();
+	eflg |= EFLAGS_AC_BIT;
+	io_store_eflags(eflg);
+	eflg = io_load_eflags();
+	if ((eflg & EFLAGS_AC_BIT) != 0) {
+		flg486 = 1;
+	}
+	eflg &= ~EFLAGS_AC_BIT;
+	io_store_eflags(eflg);
+
+	if (flg486 != 0) {
+		cr0 = load_cr0();
+		cr0 |= CR0_CACHE_DISABLE;
+		store_cr0(cr0);
+	}
+
+	i = memtest_sub(start, end);
+
+	if (flg486 != 0) {
+		cr0 = load_cr0();
+		cr0 &= ~CR0_CACHE_DISABLE;
+		store_cr0(cr0);
+	}
+
+	return i;
+}
+
+unsigned int memtest_sub(unsigned int start, unsigned int end) {
+	unsigned int i, *p, old, pat0 = 0xaa55aa55, pat1 = 0x55aa55aa;
+	for (i = start; i <= end; i += 0x1000) {
+		p = (unsigned int *)(i + 0xffc);
+		old = *p;
+		*p = pat0;
+		*p ^= 0xffffffff;
+		if (*p != pat1) {
+		not_memory:
+			*p = old;
+			break;
+		}
+		*p ^= 0xffffffff;
+		if (*p != pat0) {
+			goto not_memory;
+		}
+		*p = old;
+	}
+	return i;
 }
