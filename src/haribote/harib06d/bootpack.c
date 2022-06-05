c0ba8998d8f3e0dc5f290c795f9d86bd9451f4df
Ubugeeei <ubuge1122@gamil.com>
2022-06-05 23:34:51 +0900

    feat: impl memory management struct and constant

--- src/haribote/harib06d/bootpack.c
+++ src/haribote/harib06d/bootpack.c
@@ -1,6 +1,22 @@
 #include "bootpack.h"
 
 unsigned int memtest(unsigned int start, unsigned int end);
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
+
+#define MEMMAN_FREES 4090
+#define MEMMAN_ADDR 0x003c000
+
+struct FREEINFO {
+	unsigned int addr, size;
+};
+
+struct MEMMAN {
+	int frees, maxfrees, lostsize, losts;
+	struct FREEINFO free[MEMMAN_FREES];
+}
 
 void HariMain(void) {
 	struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
