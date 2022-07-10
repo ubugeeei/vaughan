e52cf1c51d8000c071692a0de497c58f795b1fea
Ubugeeei <ubuge1122@gamil.com>
2022-07-10 22:13:12 +0900

    refactor: dvide file (memory management)

--- src/haribote/harib07a/bootpack.h
+++ src/haribote/harib07a/bootpack.h
@@ -24,6 +24,7 @@ void store_cr0(int cr0);
 void asm_inthandler21(void);
 void asm_inthandler27(void);
 void asm_inthandler2c(void);
+unsigned int memtest_sub(unsigned int start, unsigned int end);
 
 /* fifo.c */
 struct FIFO8 {
@@ -123,5 +124,21 @@ void enable_mouse(struct MOUSE_DEC *mdec);
 int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
 extern struct FIFO8 mousefifo;
 
-/* mem test */
-unsigned int memtest_sub(unsigned int start, unsigned int end);
+/* memory.c */
+#define MEMMAN_FREES 4090
+#define MEMMAN_ADDR 0x003c0000
+struct FREEINFO {
+	unsigned int addr, size;
+};
+struct MEMMAN {
+	int frees, maxfrees, lostsize, losts;
+	struct FREEINFO free[MEMMAN_FREES];
+};
+unsigned int memtest(unsigned int start, unsigned int end);
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
+unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size);
+int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size);
+
