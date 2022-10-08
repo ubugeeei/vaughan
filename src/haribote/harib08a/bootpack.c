8569432e0435ace5302256cb4cd6193543fcb719
Ubugeeei <ubuge1122@gamil.com>
2022-10-08 16:52:46 +0900

    tweak: mouse paint

--- src/haribote/harib08a/bootpack.c
+++ src/haribote/harib08a/bootpack.c
@@ -111,13 +111,13 @@ void HariMain(void)
 					{
 						my = 0;
 					}
-					if (mx > binfo->scrnx - 16)
+					if (mx > binfo->scrnx - 1)
 					{
-						mx = binfo->scrnx - 16;
+						mx = binfo->scrnx - 1;
 					}
-					if (my > binfo->scrny - 16)
+					if (my > binfo->scrny - 1)
 					{
-						my = binfo->scrny - 16;
+						my = binfo->scrny - 1;
 					}
 
 					sprintf(s, "(%d, %d)", mx, my);
