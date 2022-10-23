5077d69f611f12d173536b925f6bf76973a08480
Ubugeeei <ubuge1122@gamil.com>
2022-10-23 22:49:15 +0900

    feat: input "A" from keyboard

--- src/haribote/harib11f/src/bootpack.c
+++ src/haribote/harib11f/src/bootpack.c
@@ -82,6 +82,10 @@ void HariMain(void) {
                 sprintf(s, "%d", i - 256);
                 putfonts8_asc_sht(sht_back, 0, 16, COL8_FFFFFF, COL8_000000, s,
                                   2);
+                if (i == 0x1e + 256) {
+                    putfonts8_asc_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6,
+                                      "A", 1);
+                }
             } else if (512 <= i && i <= 767) {  // mouse
                 if (mouse_decode(&mdec, i - 512) != 0) {
                     sprintf(s, "[lcr %d %d]", mdec.x, mdec.y);
