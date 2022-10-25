bd78d9a98aa3888915e57e22532c4c27a1be4166
Ubugeeei <ubuge1122@gamil.com>
2022-10-25 09:19:01 +0900

    feat: refresh count paint per 10ms

--- src/haribote/harib12e/src/bootpack.c
+++ src/haribote/harib12e/src/bootpack.c
@@ -12,7 +12,7 @@ struct TSS32 {
     int ldtr, iomap;
 };
 
-void task_b_main(void);
+void task_b_main(struct SHEET *sht_back);
 
 void HariMain(void) {
     struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
@@ -104,6 +104,7 @@ void HariMain(void) {
     set_segmdesc(gdt + 3, 103, (int)&tss_a, AR_TSS32);
     set_segmdesc(gdt + 4, 103, (int)&tss_b, AR_TSS32);
     load_tr(3 * 8);
+    task_b_esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024 - 8;
     task_b_esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024;
     tss_b.eip = (int)&task_b_main;
     tss_b.eflags = 0x00000202; /* IF = 1; */
@@ -121,7 +122,7 @@ void HariMain(void) {
     tss_b.ds = 1 * 8;
     tss_b.fs = 1 * 8;
     tss_b.gs = 1 * 8;
-    *((int *)0x0fec) = (int)sht_back;
+    *((int *)(task_b_esp + 4)) = (int)sht_back;
 
     for (;;) {
         io_cli();
@@ -283,23 +284,22 @@ void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c) {
     return;
 }
 
-void task_b_main(void) {
+void task_b_main(struct SHEET *sht_back) {
     struct FIFO32 fifo;
-    struct TIMER *timer_ts;
+    struct TIMER *timer_ts, *timer_put;
     int i, fifobuf[128], count = 0;
-    char s[11];
-    struct SHEET *sht_back;
+    char s[12];
 
     fifo32_init(&fifo, 128, fifobuf);
     timer_ts = timer_alloc();
-    timer_init(timer_ts, &fifo, 1);
+    timer_init(timer_ts, &fifo, 2);
     timer_settime(timer_ts, 2);
-    sht_back = (struct SHEET *)*((int *)0x0fec);
+    timer_put = timer_alloc();
+    timer_init(timer_put, &fifo, 1);
+    timer_settime(timer_put, 1);
 
     for (;;) {
         count++;
-        sprintf(s, "%d", count);
-        putfonts8_asc_sht(sht_back, 0, 144, COL8_FFFFFF, COL8_000000, s, 10);
         io_cli();
         if (fifo32_status(&fifo) == 0) {
             io_sti();
@@ -308,6 +308,11 @@ void task_b_main(void) {
             i = fifo32_get(&fifo);
             io_sti();
             if (i == 1) {
+                sprintf(s, "%d", count);
+                putfonts8_asc_sht(sht_back, 0, 144, COL8_FFFFFF, COL8_000000, s,
+                                  11);
+                timer_settime(timer_put, 1);
+            } else if (i == 2) {
                 farjmp(0, 3 * 8);
                 timer_settime(timer_ts, 2);
             }
