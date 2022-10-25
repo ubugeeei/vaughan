6cd3d74b1feefea6842411d330bcd839a54238f1
Ubugeeei <ubuge1122@gamil.com>
2022-10-25 16:23:14 +0900

    feat: task controll

--- src/haribote/harib13a/src/timer.c
+++ src/haribote/harib13a/src/timer.c
@@ -91,7 +91,7 @@ void inthandler20(int *esp) {
             break;
         }
         timer->flags = TIMER_FLAGS_ALLOC;
-        if (timer != mt_timer) {
+        if (timer != task_timer) {
             fifo32_put(timer->fifo, timer->data);
         } else {
             ts = 1;
@@ -101,7 +101,7 @@ void inthandler20(int *esp) {
     timerctl.t0 = timer;
     timerctl.next = timer->timeout;
     if (ts != 0) {
-        mt_taskswitch();
+        task_switch();
     }
     return;
 }
