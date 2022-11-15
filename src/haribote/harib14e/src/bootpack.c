8614ebdd5d370521e240a3a5a8e94ad4333161ac
Ubugeeei <ubuge1122@gamil.com>
2022-11-15 23:30:46 +0900

    feat: input shifted key

--- src/haribote/harib14e/src/bootpack.c
+++ src/haribote/harib14e/src/bootpack.c
@@ -18,18 +18,33 @@ void HariMain(void) {
     unsigned int memtotal;
     struct MOUSE_DEC mdec;
     struct MEMMAN *memman = (struct MEMMAN *)MEMMAN_ADDR;
-    static char keytable[0x54] = {
-        0, 0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^',
-        0, 0,   'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '@', '[',
-        0, 0,   'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', ':', 0,
-        0, ']', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 0,   '*',
-        0, ' ', 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-        0, '7', '8', '9', '-', '4', '5', '6', '+', '1', '2', '3', '0', '.'};
     unsigned char *buf_back, buf_mouse[256], *buf_win, *buf_cons;
     struct SHEET *sht_back, *sht_mouse, *sht_win, *sht_cons;
     struct TASK *task_a, *task_cons;
     struct TIMER *timer;
-    int key_to = 0;
+    static char keytable0[0x80] = {
+        0,   0,    '1', '2', '3', '4', '5', '6', '7',  '8', '9', '0',  '-',
+        '=', 0,    0,   'q', 'w', 'e', 'r', 't', 'y',  'u', 'i', 'o',  'p',
+        '[', ']',  0,   0,   'a', 's', 'd', 'f', 'g',  'h', 'j', 'k',  'l',
+        ';', '\'', 0,   0,   ']', 'z', 'x', 'c', 'v',  'b', 'n', 'm',  ',',
+        '.', '/',  0,   '*', 0,   ' ', 0,   0,   0,    0,   0,   0,    0,
+        0,   0,    0,   0,   0,   0,   '7', '8', '9',  '-', '4', '5',  '6',
+        '+', '1',  '2', '3', '0', '.', 0,   0,   0,    0,   0,   0,    0,
+        0,   0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,
+        0,   0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0x5c, 0,
+        0,   0,    0,   0,   0,   0,   0,   0,   0x5c, 0,   0};
+    static char keytable1[0x80] = {
+        0,   0,   '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_',
+        '+', '|', 0,   'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+        '{', '}', 0,   0,   'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
+        ':', '"', 0,   0,   '}', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<',
+        '>', '?', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,   0,
+        0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6',
+        '+', '1', '2', '3', '0', '.', 0,   0,   0,   0,   0,   0,   0,
+        0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+        0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   '_', 0,
+        0,   0,   0,   0,   0,   0,   0,   0,   '|', 0,   0};
+    int key_to = 0, key_shift = 0;
 
     init_gdtidt();
     init_pic();
@@ -118,17 +133,25 @@ void HariMain(void) {
                 sprintf(s, "%x", i - 256);
                 putfonts8_asc_sht(sht_back, 0, 16, COL8_FFFFFF, COL8_000000, s,
                                   2);
-                if (i < 256 + 0x54 && keytable[i - 256] != 0) {
+                if (i < 0x80 + 256) {
+                    if (key_shift == 0) {
+                        s[0] = keytable0[i - 256];
+                    } else {
+                        s[0] = keytable1[i - 256];
+                    }
+                } else {
+                    s[0] = 0;
+                }
+                if (s[0] != 0) {
                     if (key_to == 0) {
                         if (cursor_x < 128) {
-                            s[0] = keytable[i - 256];
                             s[1] = 0;
                             putfonts8_asc_sht(sht_win, cursor_x, 28,
                                               COL8_000000, COL8_FFFFFF, s, 1);
                             cursor_x += 8;
                         }
                     } else {
-                        fifo32_put(&task_cons->fifo, keytable[i - 256] + 256);
+                        fifo32_put(&task_cons->fifo, s[0] + 256);
                     }
                 }
                 if (i == 256 + 0x0e) {
@@ -154,6 +177,18 @@ void HariMain(void) {
                     }
                     sheet_refresh(sht_win, 0, 0, sht_win->bxsize, 21);
                     sheet_refresh(sht_cons, 0, 0, sht_cons->bxsize, 21);
+                }
+                if (i == 256 + 0x2a) {
+                    key_shift |= 1;
+                }
+                if (i == 256 + 0x36) {
+                    key_shift |= 2;
+                }
+                if (i == 256 + 0xaa) {
+                    key_shift &= ~1;
+                }
+                if (i == 256 + 0xb6) {
+                    key_shift &= ~2;
                 }
 
                 boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28,
