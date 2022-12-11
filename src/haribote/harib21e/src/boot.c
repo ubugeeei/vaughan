a6340a3eefe9fc6b43ae5cd1709c923dbd19a50e
Ubugeeei <ubuge1122@gamil.com>
2022-12-11 21:13:28 +0900

    wip: copy from prev section

---
+++ src/haribote/harib21e/src/boot.c
@@ -0,0 +1,340 @@
+#include "boot.h"
+
+#define KEYCMD_LED 0xed
+
+void Boot(void) {
+    struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
+    struct SHTCTL *shtctl;
+    char s[40];
+    struct Queue32 queue, keycmd;
+    int queue_buf[128], keycmd_buf[32];
+    int mx, my, i, cursor_x, cursor_c;
+    unsigned int memtotal;
+    struct MOUSE_DEC mdec;
+    struct MEMMAN *memman = (struct MEMMAN *)MEMMAN_ADDR;
+    unsigned char *buf_back, buf_mouse[256], *buf_win, *buf_cons;
+    struct SHEET *sht_back, *sht_mouse, *sht_win, *sht_cons;
+    struct TASK *task_a, *task_cons;
+    struct TIMER *timer;
+    static char keytable0[0x80] = {
+        0,   0,    '1', '2', '3', '4', '5', '6', '7',  '8', '9', '0',  '-',
+        '=', 0,    0,   'Q', 'W', 'E', 'R', 'T', 'Y',  'U', 'I', 'O',  'P',
+        '[', ']',  0,   0,   'A', 'S', 'D', 'F', 'G',  'H', 'J', 'K',  'L',
+        ';', '\'', 0,   0,   ']', 'Z', 'X', 'C', 'V',  'B', 'N', 'M',  ',',
+        '.', '/',  0,   '*', 0,   ' ', 0,   0,   0,    0,   0,   0,    0,
+        0,   0,    0,   0,   0,   0,   '7', '8', '9',  '-', '4', '5',  '6',
+        '+', '1',  '2', '3', '0', '.', 0,   0,   0,    0,   0,   0,    0,
+        0,   0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,
+        0,   0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0x5c, 0,
+        0,   0,    0,   0,   0,   0,   0,   0,   0x5c, 0,   0};
+    static char keytable1[0x80] = {
+        0,   0,   '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_',
+        '+', '|', 0,   'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+        '{', '}', 0,   0,   'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
+        ':', '"', 0,   0,   '}', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<',
+        '>', '?', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,   0,
+        0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6',
+        '+', '1', '2', '3', '0', '.', 0,   0,   0,   0,   0,   0,   0,
+        0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+        0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   '_', 0,
+        0,   0,   0,   0,   0,   0,   0,   0,   '|', 0,   0};
+    int key_to = 0, key_shift = 0, key_leds = (binfo->leds >> 4) & 7,
+        keycmd_wait = -1;
+    struct CONSOLE *cons;
+    int j, x, y, mmx = -1, mmy = -1;
+    struct SHEET *sht = 0;
+
+    init_gdtidt();
+    init_pic();
+    io_sti();
+    queue32_init(&queue, 128, queue_buf, 0);
+    init_pit();
+    init_keyboard(&queue, 256);
+    enable_mouse(&queue, 512, &mdec);
+    io_out8(PIC0_IMR, 0xf8);
+    io_out8(PIC1_IMR, 0xef);
+    queue32_init(&keycmd, 32, keycmd_buf, 0);
+
+    memtotal = memtest(0x00400000, 0xbfffffff);
+    memman_init(memman);
+    memman_free(memman, 0x00001000, 0x0009e000);
+    memman_free(memman, 0x00400000, memtotal - 0x00400000);
+
+    init_palette();
+    shtctl = shtctl_init(memman, binfo->vram, binfo->scrnx, binfo->scrny);
+    task_a = task_init(memman);
+    queue.task = task_a;
+    task_run(task_a, 1, 2);
+    *((int *)0x0fe4) = (int)shtctl;
+
+    sht_back = sheet_alloc(shtctl);
+    buf_back =
+        (unsigned char *)memman_alloc_4k(memman, binfo->scrnx * binfo->scrny);
+    sheet_setbuf(sht_back, buf_back, binfo->scrnx, binfo->scrny, -1);
+    init_screen8(buf_back, binfo->scrnx, binfo->scrny);
+
+    sht_cons = sheet_alloc(shtctl);
+    buf_cons = (unsigned char *)memman_alloc_4k(memman, 256 * 165);
+    sheet_setbuf(sht_cons, buf_cons, 256, 165, -1);
+    make_window8(buf_cons, 256, 165, "console", 0);
+    make_textbox8(sht_cons, 8, 28, 240, 128, COL8_000000);
+    task_cons = task_alloc();
+    task_cons->tss.esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024 - 12;
+    task_cons->tss.eip = (int)&console_task;
+    task_cons->tss.es = 1 * 8;
+    task_cons->tss.cs = 2 * 8;
+    task_cons->tss.ss = 1 * 8;
+    task_cons->tss.ds = 1 * 8;
+    task_cons->tss.fs = 1 * 8;
+    task_cons->tss.gs = 1 * 8;
+    *((int *)(task_cons->tss.esp + 4)) = (int)sht_cons;
+    *((int *)(task_cons->tss.esp + 8)) = memtotal;
+    task_run(task_cons, 2, 2);
+
+    sht_win = sheet_alloc(shtctl);
+    buf_win = (unsigned char *)memman_alloc_4k(memman, 160 * 52);
+    sheet_setbuf(sht_win, buf_win, 144, 52, -1);
+    make_window8(buf_win, 144, 52, "task_a", 1);
+    make_textbox8(sht_win, 8, 28, 128, 16, COL8_FFFFFF);
+    cursor_x = 8;
+    cursor_c = COL8_FFFFFF;
+    timer = timer_alloc();
+    timer_init(timer, &queue, 1);
+    timer_settime(timer, 50);
+
+    sht_mouse = sheet_alloc(shtctl);
+    sheet_setbuf(sht_mouse, buf_mouse, 16, 16, 99);
+    init_mouse_cursor8(buf_mouse, 99);
+    mx = (binfo->scrnx - 16) / 2;
+    my = (binfo->scrny - 28 - 16) / 2;
+
+    sheet_slide(sht_back, 0, 0);
+    sheet_slide(sht_cons, 32, 4);
+    sheet_slide(sht_win, 64, 56);
+    sheet_slide(sht_mouse, mx, my);
+    sheet_updown(sht_back, 0);
+    sheet_updown(sht_cons, 1);
+    sheet_updown(sht_win, 2);
+    sheet_updown(sht_mouse, 3);
+
+    queue32_put(&keycmd, KEYCMD_LED);
+    queue32_put(&keycmd, key_leds);
+
+    for (;;) {
+        if (queue32_status(&keycmd) > 0 && keycmd_wait < 0) {
+            keycmd_wait = queue32_get(&keycmd);
+            wait_KBC_sendready();
+            io_out8(PORT_KEYDAT, keycmd_wait);
+        }
+        io_cli();
+        if (queue32_status(&queue) == 0) {
+            task_sleep(task_a);
+            io_sti();
+        } else {
+            i = queue32_get(&queue);
+            io_sti();
+            if (256 <= i && i <= 511) {
+                if (i < 0x80 + 256) {
+                    if (key_shift == 0) {
+                        s[0] = keytable0[i - 256];
+                    } else {
+                        s[0] = keytable1[i - 256];
+                    }
+                } else {
+                    s[0] = 0;
+                }
+                if ('A' <= s[0] && s[0] <= 'Z') {
+                    if (((key_leds & 4) == 0 && key_shift == 0) ||
+                        ((key_leds & 4) != 0 && key_shift != 0)) {
+                        s[0] += 0x20;
+                    }
+                }
+                if (s[0] != 0) {
+                    if (key_to == 0) {
+                        if (cursor_x < 128) {
+                            s[1] = 0;
+                            putfonts8_asc_sht(sht_win, cursor_x, 28,
+                                              COL8_000000, COL8_FFFFFF, s, 1);
+                            cursor_x += 8;
+                        }
+                    } else {
+                        queue32_put(&task_cons->queue, s[0] + 256);
+                    }
+                }
+                if (i == 256 + 0x0e) {
+                    if (key_to == 0) {
+                        if (cursor_x > 8) {
+                            putfonts8_asc_sht(sht_win, cursor_x, 28,
+                                              COL8_000000, COL8_FFFFFF, " ", 1);
+                            cursor_x -= 8;
+                        }
+                    } else {
+                        queue32_put(&task_cons->queue, 8 + 256);
+                    }
+                }
+                if (i == 256 + 0x1c) {
+                    if (key_to != 0) {
+                        queue32_put(&task_cons->queue, 10 + 256);
+                    }
+                }
+                if (i == 256 + 0x0f) {
+                    if (key_to == 0) {
+                        key_to = 1;
+                        make_wtitle8(buf_win, sht_win->bxsize, "task_a", 0);
+                        make_wtitle8(buf_cons, sht_cons->bxsize, "console", 1);
+                        cursor_c = -1;
+                        boxfill8(sht_win->buf, sht_win->bxsize, COL8_FFFFFF,
+                                 cursor_x, 28, cursor_x + 7, 43);
+                        queue32_put(&task_cons->queue, 2);
+                    } else {
+                        key_to = 0;
+                        make_wtitle8(buf_win, sht_win->bxsize, "task_a", 1);
+                        make_wtitle8(buf_cons, sht_cons->bxsize, "console", 0);
+                        cursor_c = COL8_000000;
+                        queue32_put(&task_cons->queue, 3);
+                    }
+                    sheet_refresh(sht_win, 0, 0, sht_win->bxsize, 21);
+                    sheet_refresh(sht_cons, 0, 0, sht_cons->bxsize, 21);
+                }
+                if (i == 256 + 0x2a) {
+                    key_shift |= 1;
+                }
+                if (i == 256 + 0x36) {
+                    key_shift |= 2;
+                }
+                if (i == 256 + 0xaa) {
+                    key_shift &= ~1;
+                }
+                if (i == 256 + 0xb6) {
+                    key_shift &= ~2;
+                }
+                if (i == 256 + 0x3a) {
+                    key_leds ^= 4;
+                    queue32_put(&keycmd, KEYCMD_LED);
+                    queue32_put(&keycmd, key_leds);
+                }
+                if (i == 256 + 0x45) {
+                    key_leds ^= 1;
+                    queue32_put(&keycmd, KEYCMD_LED);
+                    queue32_put(&keycmd, key_leds);
+                }
+                if (i == 256 + 0x46) {
+                    key_leds ^= 1;
+                    queue32_put(&keycmd, KEYCMD_LED);
+                    queue32_put(&keycmd, key_leds);
+                }
+                if (i == 256 + 0x3b && key_shift != 0 &&
+                    task_cons->tss.ss0 != 0) {
+                    cons = (struct CONSOLE *)*((int *)0x0fec);
+                    cons_putstr0(cons, "\nBreak(key) :\n");
+                    io_cli();
+                    task_cons->tss.eax = (int)&(task_cons->tss.esp0);
+                    task_cons->tss.eip = (int)asm_end_app;
+                    io_sti();
+                }
+                if (i == 256 + 0xfa) {
+                    keycmd_wait = -1;
+                }
+                if (i == 256 + 0xfe) {
+                    wait_KBC_sendready();
+                    io_out8(PORT_KEYDAT, keycmd_wait);
+                }
+
+                if (cursor_c >= 0) {
+                    boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x,
+                             28, cursor_x + 7, 43);
+                }
+
+                if (i == 256 + 0x58 && shtctl->top > 2) {
+                    sheet_updown(shtctl->sheets[1], shtctl->top - 1);
+                }
+
+                sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
+            } else if (512 <= i && i <= 767) {
+                if (mouse_decode(&mdec, i - 512) != 0) {
+                    mx += mdec.x;
+                    my += mdec.y;
+
+                    if (mx < 0) {
+                        mx = 0;
+                    }
+                    if (my < 0) {
+                        my = 0;
+                    }
+                    if (mx > binfo->scrnx - 1) {
+                        mx = binfo->scrnx - 1;
+                    }
+                    if (my > binfo->scrny - 1) {
+                        my = binfo->scrny - 1;
+                    }
+
+                    sheet_slide(sht_mouse, mx, my);
+                    if ((mdec.btn & 0x01) != 0) {
+                        if (mmx < 0) {
+                            for (j = shtctl->top - 1; j > 0; j--) {
+                                sht = shtctl->sheets[j];
+                                x = mx - sht->vx0;
+                                y = my - sht->vy0;
+                                if (0 <= x && x < sht->bxsize && 0 <= y &&
+                                    y < sht->bysize) {
+                                    if (sht->buf[y * sht->bxsize + x] !=
+                                        sht->col_inv) {
+                                        sheet_updown(sht, shtctl->top - 1);
+                                        if (3 <= x && x < sht->bxsize - 3 &&
+                                            3 <= y && y < 21) {
+                                            mmx = mx;
+                                            mmy = my;
+                                        }
+                                        if (sht->bxsize - 21 <= x &&
+                                            x < sht->bxsize - 5 && 5 <= y &&
+                                            y < 19) {
+                                            if (sht->task != 0) {
+                                                cons = (struct CONSOLE *)*(
+                                                    (int *)0x0fec);
+                                                cons_putstr0(
+                                                    cons, "\nBreak(mouse) :\n");
+                                                io_cli();
+                                                task_cons->tss.eax =
+                                                    (int)&(task_cons->tss.esp0);
+                                                task_cons->tss.eip =
+                                                    (int)asm_end_app;
+                                                io_sti();
+                                            }
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+                        } else {
+                            x = mx - mmx;
+                            y = my - mmy;
+                            sheet_slide(sht, sht->vx0 + x, sht->vy0 + y);
+                            mmx = mx;
+                            mmy = my;
+                        }
+                    } else {
+                        mmx = -1;
+                    }
+                }
+            } else if (i <= 1) {
+                if (i != 0) {
+                    timer_init(timer, &queue, 0);
+                    if (cursor_c >= 0) {
+                        cursor_c = COL8_000000;
+                    }
+                } else {
+                    timer_init(timer, &queue, 1);
+                    if (cursor_c >= 0) {
+                        cursor_c = COL8_FFFFFF;
+                    }
+                }
+                timer_settime(timer, 50);
+                if (cursor_c >= 0) {
+                    boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x,
+                             28, cursor_x + 7, 43);
+                    sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
+                }
+            }
+        }
+    }
+}
