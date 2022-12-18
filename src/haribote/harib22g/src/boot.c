faa5b98483c5de47855a88dbae00eaab43a3db2e
Ubugeeei <ubuge1122@gamil.com>
2022-12-18 17:20:02 +0900

    refactor

--- src/haribote/harib22g/src/boot.c
+++ src/haribote/harib22g/src/boot.c
@@ -2,8 +2,8 @@
 
 #define KEYCMD_LED 0xed
 
-int keywin_off(struct SHEET *key_win, struct SHEET *sht_win, int cur_c,
-               int cur_x);
+// clang-format off
+int keywin_off(struct SHEET *key_win, struct SHEET *sht_win, int cur_c, int cur_x);
 int keywin_on(struct SHEET *key_win, struct SHEET *sht_win, int cur_c);
 
 void Boot(void) {
@@ -42,8 +42,7 @@ void Boot(void) {
         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   '_', 0,
         0,   0,   0,   0,   0,   0,   0,   0,   '|', 0,   0};
-    int key_to = 0, key_shift = 0, key_leds = (binfo->leds >> 4) & 7,
-        keycmd_wait = -1;
+    int key_to = 0, key_shift = 0, key_leds = (binfo->leds >> 4) & 7, keycmd_wait = -1;
     struct CONSOLE *cons;
     int j, x, y, mmx = -1, mmy = -1;
     struct SHEET *sht = 0, *key_win;
@@ -59,6 +58,11 @@ void Boot(void) {
     io_out8(PIC1_IMR, 0xef);
     queue32_init(&keycmd, 32, keycmd_buf, 0);
 
+    /*
+     *
+     * memman
+     *
+     */
     memtotal = memtest(0x00400000, 0xbfffffff);
     memman_init(memman);
     memman_free(memman, 0x00001000, 0x0009e000);
@@ -71,9 +75,17 @@ void Boot(void) {
     task_run(task_a, 1, 2);
     *((int *)0x0fe4) = (int)shtctl;
 
+    /*
+     *
+     * sheets
+     *
+     */
+
+    /*
+     * sht_back
+     */
     sht_back = sheet_alloc(shtctl);
-    buf_back =
-        (unsigned char *)memman_alloc_4k(memman, binfo->scrnx * binfo->scrny);
+    buf_back = (unsigned char *)memman_alloc_4k(memman, binfo->scrnx * binfo->scrny);
     sheet_setbuf(sht_back, buf_back, binfo->scrnx, binfo->scrny, -1);
     init_screen8(buf_back, binfo->scrnx, binfo->scrny);
 
@@ -87,8 +99,7 @@ void Boot(void) {
         make_window8(buf_cons[i], 256, 165, "console", 0);
         make_textbox8(sht_cons[i], 8, 28, 240, 128, COL8_000000);
         task_cons[i] = task_alloc();
-        task_cons[i]->tss.esp =
-            memman_alloc_4k(memman, 64 * 1024) + 64 * 1024 - 12;
+        task_cons[i]->tss.esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024 - 12;
         task_cons[i]->tss.eip = (int)&console_task;
         task_cons[i]->tss.es = 1 * 8;
         task_cons[i]->tss.cs = 2 * 8;
@@ -103,6 +114,9 @@ void Boot(void) {
         sht_cons[i]->flags |= 0x20;
     }
 
+    /*
+     * sht_win
+     */
     sht_win = sheet_alloc(shtctl);
     buf_win = (unsigned char *)memman_alloc_4k(memman, 160 * 52);
     sheet_setbuf(sht_win, buf_win, 144, 52, -1);
@@ -114,12 +128,18 @@ void Boot(void) {
     timer_init(timer, &queue, 1);
     timer_settime(timer, 50);
 
+    /*
+     * sht_mouse
+     */
     sht_mouse = sheet_alloc(shtctl);
     sheet_setbuf(sht_mouse, buf_mouse, 16, 16, 99);
     init_mouse_cursor8(buf_mouse, 99);
     mx = (binfo->scrnx - 16) / 2;
     my = (binfo->scrny - 28 - 16) / 2;
 
+    /*
+     * sheet layouts
+     */
     sheet_slide(sht_back, 0, 0);
     sheet_slide(sht_cons[1], 56, 6);
     sheet_slide(sht_cons[0], 8, 2);
@@ -135,24 +155,39 @@ void Boot(void) {
     queue32_put(&keycmd, KEYCMD_LED);
     queue32_put(&keycmd, key_leds);
 
+    /*
+     *
+     * event loop
+     *
+     */
     for (;;) {
         if (queue32_status(&keycmd) > 0 && keycmd_wait < 0) {
             keycmd_wait = queue32_get(&keycmd);
             wait_KBC_sendready();
             io_out8(PORT_KEYDAT, keycmd_wait);
         }
+
         io_cli();
+
         if (queue32_status(&queue) == 0) {
+            // sleep if there is no event
             task_sleep(task_a);
             io_sti();
         } else {
+            // handle event
             i = queue32_get(&queue);
             io_sti();
             if (key_win->flags == 0) {
                 key_win = shtctl->sheets[shtctl->top - 1];
                 cursor_c = keywin_on(key_win, sht_win, cursor_c);
             }
+
             if (256 <= i && i <= 511) {
+                /*
+                 *
+                 * handle keyboard event
+                 *
+                 */
                 if (i < 0x80 + 256) {
                     if (key_shift == 0) {
                         s[0] = keytable0[i - 256];
@@ -162,12 +197,16 @@ void Boot(void) {
                 } else {
                     s[0] = 0;
                 }
+
+                // convert to lowercase
                 if ('A' <= s[0] && s[0] <= 'Z') {
                     if (((key_leds & 4) == 0 && key_shift == 0) ||
                         ((key_leds & 4) != 0 && key_shift != 0)) {
                         s[0] += 0x20;
                     }
                 }
+
+                // normal characters
                 if (s[0] != 0) {
                     if (key_win == sht_win) {
                         if (cursor_x < 128) {
@@ -180,6 +219,8 @@ void Boot(void) {
                         queue32_put(&key_win->task->queue, s[0] + 256);
                     }
                 }
+
+                // BackSpace
                 if (i == 256 + 0x0e) {
                     if (key_win == sht_win) {
                         if (cursor_x > 8) {
@@ -191,11 +232,15 @@ void Boot(void) {
                         queue32_put(&key_win->task->queue, 8 + 256);
                     }
                 }
+
+                // Enter
                 if (i == 256 + 0x1c) {
                     if (key_win != sht_win) {
                         queue32_put(&key_win->task->queue, 10 + 256);
                     }
                 }
+
+                // Tab
                 if (i == 256 + 0x0f) {
                     cursor_c = keywin_off(key_win, sht_win, cursor_c, cursor_x);
                     j = key_win->height - 1;
@@ -205,35 +250,48 @@ void Boot(void) {
                     key_win = shtctl->sheets[j];
                     cursor_c = keywin_on(key_win, sht_win, cursor_c);
                 }
+
+                // LShift ON
                 if (i == 256 + 0x2a) {
                     key_shift |= 1;
                 }
+                // RShift ON
                 if (i == 256 + 0x36) {
                     key_shift |= 2;
                 }
+                // LShift OFF
                 if (i == 256 + 0xaa) {
                     key_shift &= ~1;
                 }
+                // RShift OFF
                 if (i == 256 + 0xb6) {
                     key_shift &= ~2;
                 }
+
+                // CapsLock
                 if (i == 256 + 0x3a) {
                     key_leds ^= 4;
                     queue32_put(&keycmd, KEYCMD_LED);
                     queue32_put(&keycmd, key_leds);
                 }
+
+                // NumLock
                 if (i == 256 + 0x45) {
                     key_leds ^= 2;
                     queue32_put(&keycmd, KEYCMD_LED);
                     queue32_put(&keycmd, key_leds);
                 }
+
+                // ScrollLock
                 if (i == 256 + 0x46) {
                     key_leds ^= 1;
                     queue32_put(&keycmd, KEYCMD_LED);
                     queue32_put(&keycmd, key_leds);
                 }
-                if (i == 256 + 0x3b && key_shift != 0 &&
-                    task_cons[0]->tss.ss0 != 0) {  // Shift + F1
+
+                // Shift + F1
+                // clang-format off
+                if (i == 256 + 0x3b && key_shift != 0 && task_cons[0]->tss.ss0 != 0) {
                     cons = (struct CONSOLE *)*((int *)0x0fec);
                     cons_putstr0(cons, "\nBreak(key) :\n");
                     io_cli();
@@ -241,24 +299,37 @@ void Boot(void) {
                     task_cons[0]->tss.eip = (int)asm_end_app;
                     io_sti();
                 }
+
+                // F12
                 if (i == 256 + 0x58 && shtctl->top > 2) {
                     sheet_updown(shtctl->sheets[1], shtctl->top - 1);
                 }
+
+                // if received data
                 if (i == 256 + 0xfa) {
                     keycmd_wait = -1;
                 }
+
+                // if dont receive data
                 if (i == 256 + 0xfe) {
                     wait_KBC_sendready();
                     io_out8(PORT_KEYDAT, keycmd_wait);
                 }
 
+                // redraw cursor
                 if (cursor_c >= 0) {
-                    boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x,
-                             28, cursor_x + 7, 43);
+                    // clang-format off
+                    boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+                } else {
+                    // do nothing
                 }
-
                 sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
             } else if (512 <= i && i <= 767) {
+                /* 
+                 * 
+                 * handle mouse event
+                 * 
+                */
                 if (mouse_decode(&mdec, i - 512) != 0) {
                     mx += mdec.x;
                     my += mdec.y;
@@ -283,38 +354,35 @@ void Boot(void) {
                                 sht = shtctl->sheets[j];
                                 x = mx - sht->vx0;
                                 y = my - sht->vy0;
-                                if (0 <= x && x < sht->bxsize && 0 <= y &&
-                                    y < sht->bysize) {
-                                    if (sht->buf[y * sht->bxsize + x] !=
-                                        sht->col_inv) {
+
+                                // clang-format off
+                                if (0 <= x && x < sht->bxsize && 0 <= y && y < sht->bysize) {
+                                    // clang-format off
+                                    if (sht->buf[y * sht->bxsize + x] != sht->col_inv) {
                                         if (sht != key_win) {
-                                            cursor_c =
-                                                keywin_off(key_win, sht_win,
-                                                           cursor_c, cursor_x);
+                                            // clang-format off
+                                            cursor_c = keywin_off(key_win, sht_win, cursor_c, cursor_x);
                                             key_win = sht;
-                                            cursor_c = keywin_on(
-                                                key_win, sht_win, cursor_c);
+                                            // clang-format off
+                                            cursor_c = keywin_on(key_win, sht_win, cursor_c);
                                         }
                                         sheet_updown(sht, shtctl->top - 1);
-                                        if (3 <= x && x < sht->bxsize - 3 &&
-                                            3 <= y && y < 21) {
+
+                                        // clang-format off
+                                        if (3 <= x && x < sht->bxsize - 3 && 3 <= y && y < 21) {
                                             mmx = mx;
                                             mmy = my;
                                         }
 
-                                        if (sht->bxsize - 21 <= x &&
-                                            x < sht->bxsize - 5 && 5 <= y &&
-                                            y < 19) {
+                                        // clang-format off
+                                        if (sht->bxsize - 21 <= x && x < sht->bxsize - 5 && 5 <= y && y < 19) {
                                             if ((sht->flags & 0x10) != 0) {
-                                                cons = (struct CONSOLE *)*(
-                                                    (int *)0x0fec);
-                                                cons_putstr0(
-                                                    cons, "\nBreak(mouse) :\n");
+                                                // clang-format off
+                                                cons = (struct CONSOLE *)*((int *)0x0fec);
+                                                cons_putstr0(cons, "\nBreak(mouse) :\n");
                                                 io_cli();
-                                                task_cons[0]->tss.eax = (int)&(
-                                                    task_cons[0]->tss.esp0);
-                                                task_cons[0]->tss.eip =
-                                                    (int)asm_end_app;
+                                                task_cons[0]->tss.eax = (int)&(task_cons[0]->tss.esp0);
+                                                task_cons[0]->tss.eip = (int)asm_end_app;
                                                 io_sti();
                                             }
                                         }
@@ -334,6 +402,11 @@ void Boot(void) {
                     }
                 }
             } else if (i <= 1) {
+                /*
+                 * 
+                 * cursor blink
+                 * 
+                 */
                 if (i != 0) {
                     timer_init(timer, &queue, 0);
                     if (cursor_c >= 0) {
@@ -347,8 +420,7 @@ void Boot(void) {
                 }
                 timer_settime(timer, 50);
                 if (cursor_c >= 0) {
-                    boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x,
-                             28, cursor_x + 7, 43);
+                    boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
                     sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
                 }
             }
@@ -356,13 +428,11 @@ void Boot(void) {
     }
 }
 
-int keywin_off(struct SHEET *key_win, struct SHEET *sht_win, int cur_c,
-               int cur_x) {
+int keywin_off(struct SHEET *key_win, struct SHEET *sht_win, int cur_c, int cur_x) {
     change_wtitle8(key_win, 0);
     if (key_win == sht_win) {
         cur_c = -1;
-        boxfill8(sht_win->buf, sht_win->bxsize, COL8_FFFFFF, cur_x, 28,
-                 cur_x + 7, 43);
+        boxfill8(sht_win->buf, sht_win->bxsize, COL8_FFFFFF, cur_x, 28, cur_x + 7, 43);
     } else {
         if ((key_win->flags & 0x20) != 0) {
             queue32_put(&key_win->task->queue, 3);
