840aeace8d3cb45f64a6190efe043921bbc2795b
Ubugeeei <ubuge1122@gamil.com>
2023-01-02 23:06:57 +0900

    wip: copy from prev section

---
+++ log/haribote/harib27e/win/win.c
@@ -0,0 +1,11 @@
+#include "../apilib.h"
+
+void HariMain(void) {
+	int win;
+	char *buf = alloca(150 * 50);
+	win = open(buf, 150, 50, -1, "hello");
+	for (;;) {
+		if (get_key(1) == 0x0a) { break; }
+	}
+	api_end();
+}
